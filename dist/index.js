b576add19f594d9aa619a3edb9caac6739071956
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-03-18 18:20:48 +0100

    Index file

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,9 @@
-import { setBoard } from "./procs.js";
-const boardElement = document.querySelector("#boardContainer");
-const board = new Array(64);
+import { setBoard, waitForClick } from "./procs.js";
+export const boardElement = document.querySelector("#boardContainer");
+export const board = new Array(64);
 setBoard(boardElement, board);
-function waitForClick(event) {
-    let target = event.target;
-    if (target.tagName === "BUTTON" && !target.children[0])
-        return;
-    if (target.tagName === "IMG") {
-        target.style.opacity = "40%";
-        if (!target.parentElement)
-            throw new Error("target.parentElement is null");
-        target = target.parentElement;
-    }
-    else {
-        let child = target.children[0];
-        child.style.opacity = "40%";
-    }
-    boardElement.removeEventListener("click", waitForClick);
-    function clickReact(event) {
-        let dest = event.target;
-        if (dest.tagName === "IMG") {
-            if (!dest.parentElement)
-                throw new Error("dest.parentElement is null");
-            dest = dest.parentElement;
-        }
-        afterClick(target, dest);
-        boardElement.removeEventListener("click", clickReact);
-        boardElement.addEventListener("click", waitForClick);
-    }
-    boardElement.addEventListener("click", clickReact);
-}
-function afterClick(src, dest) {
-    if (board[Number(src.id)].pieceColor === board[Number(dest.id)].pieceColor)
-        console.log("same color");
-    else
-        console.log("not same color");
-    let img = src.children[0];
-    img.style.opacity = "1";
-}
 boardElement.addEventListener("click", waitForClick);
+self.addEventListener("message", (message) => {
+    if (message.data === "add clickReact")
+        boardElement.addEventListener("click", waitForClick);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,13 @@
-import {setBoard, Cell} from "./procs.js"
+import {Cell, setBoard, waitForClick} from "./procs.js"
 
-const boardElement = document.querySelector("#boardContainer") as HTMLDivElement
-const board: Cell[] = new Array(64)
+export const boardElement = document.querySelector("#boardContainer") as HTMLDivElement
+export const board: Cell[] = new Array(64)
 
 setBoard(boardElement, board)
 
-function waitForClick(event: MouseEvent) {
-    let target = event.target as HTMLElement
-    if(target.tagName === "BUTTON" && !target.children[0])
-        return
-
-    if(target.tagName === "IMG"){
-        target.style.opacity = "40%"
-        
-        if(!target.parentElement)
-            throw new Error("target.parentElement is null");
-        
-        target = target.parentElement as HTMLButtonElement
-    }
-    else{
-        let child = target.children[0] as HTMLImageElement
-        child.style.opacity = "40%"
-    }
-
-    boardElement.removeEventListener("click", waitForClick)
-
-    function clickReact(event: MouseEvent) {
-        let dest = event.target as HTMLElement
-        if(dest.tagName === "IMG"){
-            if(!dest.parentElement)
-                throw new Error("dest.parentElement is null");
-    
-            dest = dest.parentElement as HTMLButtonElement
-        }
-        afterClick(target as HTMLButtonElement, dest as HTMLButtonElement)
-        boardElement.removeEventListener("click", clickReact)
-        boardElement.addEventListener("click", waitForClick)
-    }
-
-    boardElement.addEventListener("click", clickReact)
-
-    
-}
-
-function afterClick(src: HTMLButtonElement, dest: HTMLButtonElement) {
-    if(board[Number(src.id)].pieceColor === board[Number(dest.id)].pieceColor)
-        console.log("same color")
-    else
-        console.log("not same color")
-    
-    let img = src.children[0] as HTMLImageElement
-    img.style.opacity = "1"
-}
-
 boardElement.addEventListener("click", waitForClick)
 
-
-
-
+self.addEventListener("message", (message) => {
+    if(message.data === "add clickReact")
+        boardElement.addEventListener("click", waitForClick) 
+})
