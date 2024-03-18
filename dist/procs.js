e267f0e5effee7092d4cd9b75bf25b950b3d8069
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-03-18 18:20:23 +0100

    Functions that will be used in index file
    Shoved all extra functons into the procs file, still not sure if I will keep it that way, because currenntly the index file is too empty.
    And still need to write the functions getLegalMoves and complete isLegal.

--- dist/procs.js
+++ dist/procs.js
@@ -1,26 +1,27 @@
+import { boardElement, board } from "./index.js";
 var cellColor;
 (function (cellColor) {
     cellColor["white"] = "white";
     cellColor["black"] = "black";
 })(cellColor || (cellColor = {}));
-var pieceValues;
-(function (pieceValues) {
-    pieceValues[pieceValues["empty"] = 0] = "empty";
-    pieceValues[pieceValues["pawn"] = 1] = "pawn";
-    pieceValues[pieceValues["knight"] = 3] = "knight";
-    pieceValues[pieceValues["bishop"] = 3] = "bishop";
-    pieceValues[pieceValues["rook"] = 5] = "rook";
-    pieceValues[pieceValues["queen"] = 9] = "queen";
-    pieceValues[pieceValues["king"] = 15] = "king";
-})(pieceValues || (pieceValues = {}));
-var colorValue;
-(function (colorValue) {
-    colorValue[colorValue["empty"] = 0] = "empty";
-    colorValue[colorValue["white"] = 1] = "white";
-    colorValue[colorValue["black"] = 2] = "black";
-})(colorValue || (colorValue = {}));
-function setCell(row, color, value, pieceColor, element) {
-    return { row: row, color: color, value: value, pieceColor: pieceColor, element: element };
+var cellValue;
+(function (cellValue) {
+    cellValue[cellValue["empty"] = 0] = "empty";
+    cellValue[cellValue["wPawn"] = 17] = "wPawn";
+    cellValue[cellValue["wKnight"] = 19] = "wKnight";
+    cellValue[cellValue["wBishop"] = 20] = "wBishop";
+    cellValue[cellValue["wRook"] = 21] = "wRook";
+    cellValue[cellValue["wQueen"] = 25] = "wQueen";
+    cellValue[cellValue["wKing"] = 31] = "wKing";
+    cellValue[cellValue["bPawn"] = 33] = "bPawn";
+    cellValue[cellValue["bKnight"] = 35] = "bKnight";
+    cellValue[cellValue["bBishop"] = 36] = "bBishop";
+    cellValue[cellValue["bRook"] = 37] = "bRook";
+    cellValue[cellValue["bQueen"] = 41] = "bQueen";
+    cellValue[cellValue["bKing"] = 47] = "bKing";
+})(cellValue || (cellValue = {}));
+function setCell(row, color, value, element) {
+    return { row: row, color: color, value: value, element: element };
 }
 function setRow(row, board, rowIndex) {
     if (rowIndex >= 2 && rowIndex <= 7) {
@@ -28,11 +29,11 @@ function setRow(row, board, rowIndex) {
             let newCell = document.createElement("button");
             newCell.id = String((rowIndex - 1) * 8 + i);
             newCell.className = (rowIndex % 2) ? "cell white" : "cell black";
-            board[(rowIndex - 1) * 8 + i] = setCell(rowIndex, (rowIndex % 2) ? cellColor.black : cellColor.white, (rowIndex === 2 || rowIndex === 7) ? pieceValues.pawn : pieceValues.empty, (rowIndex === 2) ? colorValue.white : (rowIndex === 7) ? colorValue.black : colorValue.empty, newCell);
+            board[(rowIndex - 1) * 8 + i] = setCell(rowIndex, (rowIndex % 2) ? cellColor.black : cellColor.white, (rowIndex === 2) ? cellValue.wPawn : (rowIndex === 7) ? cellValue.bPawn : cellValue.empty, newCell);
             let newCell2 = document.createElement("button");
             newCell2.id = String((rowIndex - 1) * 8 + i + 1);
             newCell2.className = (rowIndex % 2) ? "cell black" : "cell white";
-            board[(rowIndex - 1) * 8 + i + 1] = setCell(rowIndex, (rowIndex % 2) ? cellColor.white : cellColor.black, (rowIndex === 2 || rowIndex === 7) ? pieceValues.pawn : pieceValues.empty, (rowIndex === 2) ? colorValue.white : (rowIndex === 7) ? colorValue.black : colorValue.empty, newCell2);
+            board[(rowIndex - 1) * 8 + i + 1] = setCell(rowIndex, (rowIndex % 2) ? cellColor.white : cellColor.black, (rowIndex === 2) ? cellValue.wPawn : (rowIndex === 7) ? cellValue.bPawn : cellValue.empty, newCell2);
             newCell.innerHTML = newCell2.innerHTML = (rowIndex === 2) ? `<image src="images/pieces/white-pawn.png">` : (rowIndex === 7) ? `<image src="images/pieces/black-pawn.png">` : "";
             row.append(newCell, newCell2);
         }
@@ -42,42 +43,42 @@ function setRow(row, board, rowIndex) {
     Cell1.id = String((rowIndex - 1) * 8 + 0);
     Cell1.className = (rowIndex === 1) ? "cell white" : "cell black";
     Cell1.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-rook.png">` : `<image src="images/pieces/black-rook.png">`;
-    board[(rowIndex - 1) * 8 + 0] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, pieceValues.rook, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell1);
+    board[(rowIndex - 1) * 8 + 0] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, (rowIndex === 1) ? cellValue.wRook : cellValue.bRook, Cell1);
     let Cell2 = document.createElement("button");
     Cell2.id = String((rowIndex - 1) * 8 + 1);
     Cell2.className = (rowIndex === 1) ? "cell black" : "cell white";
     Cell2.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-knight.png">` : `<image src="images/pieces/black-knight.png">`;
-    board[(rowIndex - 1) * 8 + 1] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, pieceValues.knight, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell2);
+    board[(rowIndex - 1) * 8 + 1] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, (rowIndex === 1) ? cellValue.wKnight : cellValue.bKnight, Cell2);
     let Cell3 = document.createElement("button");
     Cell3.id = String((rowIndex - 1) * 8 + 2);
     Cell3.className = (rowIndex === 1) ? "cell white" : "cell black";
     Cell3.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-bishop.png">` : `<image src="images/pieces/black-bishop.png">`;
-    board[(rowIndex - 1) * 8 + 2] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, pieceValues.bishop, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell3);
+    board[(rowIndex - 1) * 8 + 2] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, (rowIndex === 1) ? cellValue.wBishop : cellValue.bBishop, Cell3);
     let Cell4 = document.createElement("button");
     Cell4.id = String((rowIndex - 1) * 8 + 3);
     Cell4.className = (rowIndex === 1) ? "cell black" : "cell white";
     Cell4.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-king.png">` : `<image src="images/pieces/black-king.png">`;
-    board[(rowIndex - 1) * 8 + 3] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, pieceValues.king, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell4);
+    board[(rowIndex - 1) * 8 + 3] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, (rowIndex === 1) ? cellValue.wKing : cellValue.bKing, Cell4);
     let Cell5 = document.createElement("button");
     Cell5.id = String((rowIndex - 1) * 8 + 4);
     Cell5.className = (rowIndex === 1) ? "cell white" : "cell black";
     Cell5.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-queen.png">` : `<image src="images/pieces/black-queen.png">`;
-    board[(rowIndex - 1) * 8 + 4] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, pieceValues.queen, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell5);
+    board[(rowIndex - 1) * 8 + 4] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, (rowIndex === 1) ? cellValue.wQueen : cellValue.bQueen, Cell5);
     let Cell6 = document.createElement("button");
     Cell6.id = String((rowIndex - 1) * 8 + 5);
     Cell6.className = (rowIndex === 1) ? "cell black" : "cell white";
     Cell6.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-bishop.png">` : `<image src="images/pieces/black-bishop.png">`;
-    board[(rowIndex - 1) * 8 + 5] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, pieceValues.bishop, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell6);
+    board[(rowIndex - 1) * 8 + 5] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, (rowIndex === 1) ? cellValue.wBishop : cellValue.bBishop, Cell6);
     let Cell7 = document.createElement("button");
     Cell7.id = String((rowIndex - 1) * 8 + 6);
     Cell7.className = (rowIndex === 1) ? "cell white" : "cell black";
     Cell7.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-knight.png">` : `<image src="images/pieces/black-knight.png">`;
-    board[(rowIndex - 1) * 8 + 6] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, pieceValues.knight, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell7);
+    board[(rowIndex - 1) * 8 + 6] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white : cellColor.black, (rowIndex === 1) ? cellValue.wKnight : cellValue.bKnight, Cell7);
     let Cell8 = document.createElement("button");
     Cell8.id = String((rowIndex - 1) * 8 + 7);
     Cell8.className = (rowIndex === 1) ? "cell black" : "cell white";
     Cell8.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-rook.png">` : `<image src="images/pieces/black-rook.png">`;
-    board[(rowIndex - 1) * 8 + 7] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, pieceValues.rook, (rowIndex === 1) ? colorValue.white : colorValue.black, Cell8);
+    board[(rowIndex - 1) * 8 + 7] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black : cellColor.white, (rowIndex === 1) ? cellValue.wRook : cellValue.bRook, Cell8);
     row.append(Cell1, Cell2, Cell3, Cell4, Cell5, Cell6, Cell7, Cell8);
 }
 export function setBoard(boardElement, board) {
@@ -88,4 +89,65 @@ export function setBoard(boardElement, board) {
         setRow(rowElement, board, rowIndex);
         boardElement.append(rowElement);
     }
+}
+function makeMove(from, to) {
+    to.value = from.value;
+    to.element.innerHTML = from.element.innerHTML;
+    from.element.innerHTML = "";
+    from.value = 0;
+    let img = to.element.children[0];
+    img.style.opacity = "1";
+}
+function getLegalMoves(cell) {
+    cell;
+    return [];
+}
+function isLegal(from, to) {
+    // STILL NEEDS TO CHECK IN CASE OF A CHECK
+    if ((from.value & 0x30) === (to.value & 0x30))
+        return false;
+    // "getLegalMoves" function still not started
+    let legalMoves = getLegalMoves(from);
+    for (let i = 0; i < legalMoves.length; i++) {
+        if (to === legalMoves[i])
+            return true;
+    }
+    return false;
+}
+function afterClick(src, dest) {
+    if (!isLegal(board[Number(src.id)], board[Number(dest.id)])) {
+        alert("Invalid move!");
+        let img = src.children[0];
+        img.style.opacity = "1";
+    }
+    else
+        makeMove(board[Number(src.id)], board[Number(dest.id)]);
+}
+export function waitForClick(event) {
+    let target = event.target;
+    if (target.tagName === "BUTTON" && !target.children[0])
+        return;
+    if (target.tagName === "IMG") {
+        target.style.opacity = "40%";
+        if (!target.parentElement)
+            throw new Error("target.parentElement is null");
+        target = target.parentElement;
+    }
+    else {
+        let child = target.children[0];
+        child.style.opacity = "40%";
+    }
+    boardElement.removeEventListener("click", waitForClick);
+    function clickReact(event) {
+        let dest = event.target;
+        if (dest.tagName === "IMG") {
+            if (!dest.parentElement)
+                throw new Error("dest.parentElement is null");
+            dest = dest.parentElement;
+        }
+        afterClick(target, dest);
+        boardElement.removeEventListener("click", clickReact);
+        window.postMessage("add clickReact");
+    }
+    boardElement.addEventListener("click", clickReact);
 }
--- src/procs.ts
+++ src/procs.ts
@@ -1,36 +1,38 @@
+import {boardElement, board} from "./index.js"
+
 enum cellColor {
     white = "white",
     black = "black"
 }
 
-enum pieceValues {
-    empty = 0,
-    pawn = 1,
-    knight = 3,
-    bishop = 3,
-    rook = 5,
-    queen = 9,
-    king = 15
-}
+enum cellValue {
+    empty   =   0x00,
 
-enum colorValue {
-    empty = 0,
-    white = 1,
-    black = 2
+    wPawn   =   0x11,
+    wKnight =   0x13,
+    wBishop =   0x14,
+    wRook   =   0x15,
+    wQueen  =   0x19,
+    wKing   =   0x1F,
+
+    bPawn   =   0x21,
+    bKnight =   0x23,
+    bBishop =   0x24,
+    bRook   =   0x25,
+    bQueen  =   0x29,
+    bKing   =   0x2F
 }
 
 export type Cell = {
     row: number
     color: cellColor
-    value: pieceValues
-    pieceColor: colorValue
+    value: cellValue
     element: HTMLButtonElement
 }
 
-function setCell(row: number, color: cellColor, value: pieceValues, pieceColor: colorValue, element: HTMLButtonElement) {
-    return  {row: row, color: color, value: value, pieceColor: pieceColor, element: element}
+function setCell(row: number, color: cellColor, value: cellValue, element: HTMLButtonElement) {
+    return  {row: row, color: color, value: value, element: element}
 }
-
 
 function setRow(row: HTMLDivElement, board: Cell[], rowIndex: number) {
     if(rowIndex >= 2 && rowIndex <= 7) {
@@ -38,12 +40,12 @@ function setRow(row: HTMLDivElement, board: Cell[], rowIndex: number) {
             let newCell = document.createElement("button")
             newCell.id = String((rowIndex - 1) * 8 + i)
             newCell.className = (rowIndex % 2) ? "cell white": "cell black"
-            board[(rowIndex - 1) * 8 + i] = setCell(rowIndex, (rowIndex % 2) ? cellColor.black: cellColor.white, (rowIndex === 2 || rowIndex === 7) ? pieceValues.pawn: pieceValues.empty, (rowIndex === 2) ? colorValue.white: (rowIndex === 7) ? colorValue.black: colorValue.empty, newCell)
+            board[(rowIndex - 1) * 8 + i] = setCell(rowIndex, (rowIndex % 2) ? cellColor.black: cellColor.white, (rowIndex === 2) ? cellValue.wPawn: (rowIndex === 7) ? cellValue.bPawn: cellValue.empty, newCell)
 
             let newCell2 = document.createElement("button")
             newCell2.id = String((rowIndex - 1) * 8 + i + 1)
             newCell2.className = (rowIndex % 2) ? "cell black": "cell white"
-            board[(rowIndex - 1) * 8 + i + 1] = setCell(rowIndex, (rowIndex % 2) ? cellColor.white: cellColor.black, (rowIndex === 2 || rowIndex === 7) ? pieceValues.pawn: pieceValues.empty, (rowIndex === 2) ? colorValue.white: (rowIndex === 7) ? colorValue.black: colorValue.empty, newCell2)
+            board[(rowIndex - 1) * 8 + i + 1] = setCell(rowIndex, (rowIndex % 2) ? cellColor.white: cellColor.black, (rowIndex === 2) ? cellValue.wPawn: (rowIndex === 7) ? cellValue.bPawn: cellValue.empty, newCell2)
 
             newCell.innerHTML = newCell2.innerHTML = (rowIndex === 2) ? `<image src="images/pieces/white-pawn.png">`: (rowIndex === 7) ? `<image src="images/pieces/black-pawn.png">`: ""
 
@@ -56,49 +58,49 @@ function setRow(row: HTMLDivElement, board: Cell[], rowIndex: number) {
     Cell1.id = String((rowIndex - 1) * 8 + 0)
     Cell1.className = (rowIndex === 1) ? "cell white": "cell black"
     Cell1.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-rook.png">`: `<image src="images/pieces/black-rook.png">`
-    board[(rowIndex - 1) * 8 + 0] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, pieceValues.rook, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell1)
+    board[(rowIndex - 1) * 8 + 0] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, (rowIndex === 1) ? cellValue.wRook: cellValue.bRook, Cell1)
 
     let Cell2 = document.createElement("button")
     Cell2.id = String((rowIndex - 1) * 8 + 1)
     Cell2.className = (rowIndex === 1) ? "cell black": "cell white"
     Cell2.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-knight.png">`: `<image src="images/pieces/black-knight.png">`
-    board[(rowIndex - 1) * 8 + 1] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, pieceValues.knight, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell2)
+    board[(rowIndex - 1) * 8 + 1] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, (rowIndex === 1) ? cellValue.wKnight: cellValue.bKnight, Cell2)
 
     let Cell3 = document.createElement("button")
     Cell3.id = String((rowIndex - 1) * 8 + 2)
     Cell3.className = (rowIndex === 1) ? "cell white": "cell black"
     Cell3.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-bishop.png">`: `<image src="images/pieces/black-bishop.png">`
-    board[(rowIndex - 1) * 8 + 2] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, pieceValues.bishop, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell3)
+    board[(rowIndex - 1) * 8 + 2] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, (rowIndex === 1) ? cellValue.wBishop: cellValue.bBishop, Cell3)
 
     let Cell4 = document.createElement("button")
     Cell4.id = String((rowIndex - 1) * 8 + 3)
     Cell4.className = (rowIndex === 1) ? "cell black": "cell white"
     Cell4.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-king.png">`: `<image src="images/pieces/black-king.png">`
-    board[(rowIndex - 1) * 8 + 3] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, pieceValues.king, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell4)
+    board[(rowIndex - 1) * 8 + 3] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, (rowIndex === 1) ? cellValue.wKing: cellValue.bKing, Cell4)
 
     let Cell5 = document.createElement("button")
     Cell5.id = String((rowIndex - 1) * 8 + 4)
     Cell5.className = (rowIndex === 1) ? "cell white": "cell black"
     Cell5.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-queen.png">`: `<image src="images/pieces/black-queen.png">`
-    board[(rowIndex - 1) * 8 + 4] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, pieceValues.queen, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell5)
+    board[(rowIndex - 1) * 8 + 4] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, (rowIndex === 1) ? cellValue.wQueen: cellValue.bQueen, Cell5)
 
     let Cell6 = document.createElement("button")
     Cell6.id = String((rowIndex - 1) * 8 + 5)
     Cell6.className = (rowIndex === 1) ? "cell black": "cell white"
     Cell6.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-bishop.png">`: `<image src="images/pieces/black-bishop.png">`
-    board[(rowIndex - 1) * 8 + 5] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, pieceValues.bishop, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell6)
+    board[(rowIndex - 1) * 8 + 5] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, (rowIndex === 1) ? cellValue.wBishop: cellValue.bBishop, Cell6)
 
     let Cell7 = document.createElement("button")
     Cell7.id = String((rowIndex - 1) * 8 + 6)
     Cell7.className = (rowIndex === 1) ? "cell white": "cell black"
     Cell7.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-knight.png">`: `<image src="images/pieces/black-knight.png">`
-    board[(rowIndex - 1) * 8 + 6] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, pieceValues.knight, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell7)
+    board[(rowIndex - 1) * 8 + 6] = setCell(rowIndex, (rowIndex === 1) ? cellColor.white: cellColor.black, (rowIndex === 1) ? cellValue.wKnight: cellValue.bKnight, Cell7)
 
     let Cell8 = document.createElement("button")
     Cell8.id = String((rowIndex - 1) * 8 + 7)
     Cell8.className = (rowIndex === 1) ? "cell black": "cell white"
     Cell8.innerHTML = (rowIndex === 1) ? `<image src="images/pieces/white-rook.png">`: `<image src="images/pieces/black-rook.png">`
-    board[(rowIndex - 1) * 8 + 7] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, pieceValues.rook, (rowIndex === 1) ? colorValue.white: colorValue.black, Cell8)
+    board[(rowIndex - 1) * 8 + 7] = setCell(rowIndex, (rowIndex === 1) ? cellColor.black: cellColor.white, (rowIndex === 1) ? cellValue.wRook: cellValue.bRook, Cell8)
 
     row.append(Cell1, Cell2, Cell3, Cell4, Cell5, Cell6, Cell7, Cell8)
 }
@@ -111,4 +113,83 @@ export function setBoard(boardElement: HTMLDivElement, board: Cell[]) {
         setRow(rowElement, board, rowIndex)
         boardElement.append(rowElement)
     }
+}
+
+function makeMove(from: Cell, to: Cell) {
+    to.value = from.value
+    to.element.innerHTML = from.element.innerHTML
+    
+    from.element.innerHTML = ""
+    from.value = 0
+
+    let img = to.element.children[0] as HTMLImageElement
+    img.style.opacity = "1"
+}
+
+function getLegalMoves(cell: Cell): Cell[] {
+    cell;
+    return []
+}
+
+function isLegal(from: Cell, to: Cell): boolean {
+    // STILL NEEDS TO CHECK IN CASE OF A CHECK
+    if( (from.value & 0x30) === (to.value & 0x30) )
+        return false
+
+    // "getLegalMoves" function still not started
+    let legalMoves: Cell[] = getLegalMoves(from)
+    for(let i = 0; i < legalMoves.length; i++){
+        if(to === legalMoves[i])
+            return true
+    }
+
+    return false
+
+}
+
+function afterClick(src: HTMLButtonElement, dest: HTMLButtonElement) {
+    if(!isLegal( board[Number(src.id)], board[Number(dest.id)] )){
+        alert("Invalid move!")
+        let img = src.children[0] as HTMLImageElement
+        img.style.opacity = "1"
+    }
+    else
+        makeMove( board[Number(src.id)], board[Number(dest.id)] )   
+}
+
+export function waitForClick(event: MouseEvent) {
+    let target = event.target as HTMLElement
+    if(target.tagName === "BUTTON" && !target.children[0])
+        return
+
+    if(target.tagName === "IMG"){
+        target.style.opacity = "40%"
+        
+        if(!target.parentElement)
+            throw new Error("target.parentElement is null");
+        
+        target = target.parentElement as HTMLButtonElement
+    }
+    else{
+        let child = target.children[0] as HTMLImageElement
+        child.style.opacity = "40%"
+    }
+
+    boardElement.removeEventListener("click", waitForClick)
+
+    function clickReact(event: MouseEvent) {
+        let dest = event.target as HTMLElement
+        if(dest.tagName === "IMG"){
+            if(!dest.parentElement)
+                throw new Error("dest.parentElement is null");
+    
+            dest = dest.parentElement as HTMLButtonElement
+        }
+        afterClick(target as HTMLButtonElement, dest as HTMLButtonElement)
+
+        boardElement.removeEventListener("click", clickReact)
+        window.postMessage("add clickReact")
+    }
+
+    boardElement.addEventListener("click", clickReact)
 }
